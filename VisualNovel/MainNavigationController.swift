7603a9a6e91fcf3ce324f0363c435c15144621a3
Daniil <krivelevdaniil@gmail.com>
2021-03-25 06:15:12 -0700

    fixed small but nasty bug. never delete launch screen!

---
+++ VisualNovel/MainNavigationController.swift
@@ -0,0 +1,19 @@
+//
+//  MainNavigationController.swift
+//  VisualNovel
+//
+//  Created by Daniil Zavodchanovich on 25.03.2021.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
