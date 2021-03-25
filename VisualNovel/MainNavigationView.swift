70d60c2c8700269f4baa3107887da073b6d5c3dd
Daniil <krivelevdaniil@gmail.com>
2021-03-25 06:58:41 -0700

    stick to the plan boys this task is tough but we will win
    status bar colour is set to dark-blue. status bar style is changed to light mode

---
+++ VisualNovel/MainNavigationView.swift
@@ -0,0 +1,21 @@
+//
+//  MainNavigationView.swift
+//  VisualNovel
+//
+//  Created by Daniil Zavodchanovich on 25.03.2021.
+//
+
+import Foundation
+import UIKit
+
+class MainNavigationView : UINavigationController{
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return .lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
