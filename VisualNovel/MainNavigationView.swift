36cc48526fbacc7743e06a56cdfdc8e110711a8e
Daniil <krivelevdaniil@gmail.com>
2021-03-29 10:47:49 -0700

    function of creating buttons done

--- VisualNovel/MainNavigationView.swift
+++ VisualNovel/MainNavigationView.swift
@@ -46,11 +46,20 @@ extension UIButton {
 extension UIViewController{
     func setButtons(buttons: [ButtonInfo]){
         let screenSize = UIScreen.main.bounds
-        let buttonHeight = (screenSize.midY / 8.0)
+        
+        let buttonHeight =
+            (((screenSize.midY / 8.0) * CGFloat(buttons.count) * 2) <= screenSize.midY)
+            ? (screenSize.midY / 8.0)
+            : screenSize.midY / CGFloat((buttons.count * 2))
+        
+        var currY = screenSize.midY + (screenSize.midY - CGFloat(buttons.count*2 - 1)*buttonHeight)/2
+        
         for i in 0..<buttons.count{
-            let button = UIButton(frame: CGRect(x: 0, y: screenSize.midY + 50, width: screenSize.width, height: buttonHeight))
+            let button = UIButton(frame: CGRect(x: 0, y: currY, width: screenSize.width, height: buttonHeight))
             button.setButtonAppearance(text: buttons[i].LabelText, color: buttons[i].Color, fontSize: buttonHeight/2)
             self.view.addSubview(button)
+            
+            currY += buttonHeight*2
         }
     }
 }
