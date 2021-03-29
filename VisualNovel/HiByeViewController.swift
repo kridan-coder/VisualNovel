6c12720447d15e1637c08dcf193f0568fcaf7059
Пользователь <polzovatel@iMac-Admin-63.local>
2021-03-29 18:37:19 +0700

    hibyeviewcontroller

---
+++ VisualNovel/HiByeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  VisualNovel
+//
+//  Created by Daniil Zavodchanovich on 25.03.2021.
+//
+
+import UIKit
+
+class HiByeViewController: UIViewController {
+
+    var MainWindowText: String!
+    var ButtonText: String!
+    var SceneButtonLeadsTo: String!
+    
+    convenience init(MainWindowText: String, ButtonText: String, SceneButtonLeadsTo: String){
+        self.init()
+        self.MainWindowText = MainWindowText
+        self.ButtonText = ButtonText
+        self.SceneButtonLeadsTo = SceneButtonLeadsTo
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+//writing this from a library
+}
+
